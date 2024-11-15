02e36f335e0a9b2f4a7db69b4b4ec585c3fe6c41
n0ea5y <n0ea5y2024@gmail.com>
2024-11-16 02:32:10 +0900

    Revert "#1ユーザ作成、ログイン方法を"
    This reverts commit 7d247f9072f11fb84703b96db52f7fda83110aa1.

--- resources/js/Pages/Auth/Register.vue
+++ resources/js/Pages/Auth/Register.vue
@@ -49,6 +49,7 @@ const submit = () => {
                     type="email"
                     class="mt-1 block w-full"
                     v-model="form.email"
+                    required
                     autocomplete="username"
                 />
 
